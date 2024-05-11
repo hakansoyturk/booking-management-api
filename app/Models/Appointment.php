a69b4c3fb70cb7fb3175a434b564c82131db8b49
hakansoyturk <hakan.syturk@gmail.com>
2024-05-12 01:30:55 +0300

    Add Appointment model

---
+++ app/Models/Appointment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+ 
+    protected $table = 'appointments';
+    
+    protected $fillable = [
+        'user_id',
+        'salon_id',
+        'google_event_id',
+        'start_time',
+        'end_time',
+    ];
+}
