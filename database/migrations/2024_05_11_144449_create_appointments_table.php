0e89569815c3f305d875fe19d2e26bc308389dfe
hakansoyturk <hakan.syturk@gmail.com>
2024-05-12 01:26:57 +0300

    Add appointments migration

---
+++ database/migrations/2024_05_11_144449_create_appointments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('salon_id')->constrained()->onDelete('cascade');
+            $table->string('google_event_id');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
